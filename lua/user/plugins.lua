c2b97756a720c4a297d1413a1119e34d297acdb2
Michael Li <mli25782@umd.edu>
2022-12-14 18:31:34 -0500

    12/14 changes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -129,6 +129,9 @@ return packer.startup(function(use)
   -- Markdown Preview
   use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 
+  -- Vim Wiki
+  use 'vimwiki/vimwiki'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
